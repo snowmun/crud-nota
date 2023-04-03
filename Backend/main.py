77944ce2c73c5f61c20e3dc45540fd94a7c15ecd
snow <smashdojo3@gmail.com>
2023-04-03 15:19:49 -0400

    se agrega cors al main

--- Backend/main.py
+++ Backend/main.py
@@ -1,11 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.noteService import router as notes_router
 from routes.userService import router as users_router
 from routes.labelService import router as label_router
-
 app = FastAPI()
+
+# Configuración de orígenes permitidos para CORS
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "https://my-app.com",
+    "https://my-app.com:8080",
+]
+
+# Agregar middleware de CORS a la aplicación
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(notes_router)
 app.include_router(users_router)
 app.include_router(label_router)
+
 

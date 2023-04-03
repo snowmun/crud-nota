209392b49806660860885027a3dfdb87428658bd
snow <smashdojo3@gmail.com>
2023-04-03 15:21:23 -0400

    se consume el servicio y se integra en home

--- Frontend/test-nota/src/views/HomeView.vue
+++ Frontend/test-nota/src/views/HomeView.vue
@@ -2,72 +2,71 @@
   <b-card>
     <h3 class="text-center">Todas las Notas</h3>
     <div class="table-responsive">
-      <b-table striped hover stacked="md" :items="items" v-if="!loading" :fields="fieldsTable" class="margin-top__20">
-        <template #cell(action)>
-          <div class="d-flex justify-content-center">
-            <b-button size="sm" variant="outline-primary" class="mr-2 custom-button">
-              <b-icon-pencil></b-icon-pencil>
-            </b-button>
-            <b-button size="sm" variant="outline-primary" class="mr-2 custom-button">
-              <b-icon-eye></b-icon-eye>
-            </b-button>
-            <b-button size="sm" variant="danger" class="custom-button">
-              <b-icon-x></b-icon-x>
-            </b-button>
-          </div>
-        </template>
-        <template>
-          <div class="text-center text-primary my-2">
-            <b-spinner align="middle"></b-spinner>
-            <strong>Cargando</strong>
-          </div>
+      <b-table striped hover stacked="md" :items="notaList" v-if="!loading" :fields="fieldsTable" class="margin-top__20">
+        <template v-slot:cell(contenido)="data">
+          <div>{{ data.value.slice(0, 20) }}{{ data.value.length > 20 ? '...' : '' }}</div>
         </template>
       </b-table>
+      <template>
+        <div v-if="loading" class="text-center text-primary my-2 text-warning">
+          <b-spinner align="middle"></b-spinner>
+          <span>Cargando</span>
+        </div>
+      </template>
     </div>
   </b-card>
 </template>
 
 <script>
+import noteServices from '@/services/note/noteService'
+
 export default {
   name: 'HomeView',
   data() {
     return {
       loading: false,
+      notaList: null,
       fieldsTable: [
-        {
-          key: 'id',
-          label: 'Id',
-          sortable: false
-        },
-        {
-          key: 'titulo',
-          label: 'Titulo',
-          sortable: false
-        },
-        {
-          key: 'usuario',
-          label: 'Usuario',
-          sortable: false
-        },
-        {
-          key: 'estado',
-          label: 'Estado',
-          sortable: false
-        },
-        {
-          key: 'end_date',
-          label: 'Fecha Termino',
-          sortable: false
-        },
+        { key: 'index', label: '#', sortable: false },
+        { key: 'titulo', label: 'Titulo', sortable: false },
+        { key: 'contenido', label: 'Contenido', sortable: false},
+        { key: 'nombre_usuario', label: 'Usuario', sortable: false },
+        { key: 'estado', label: 'Estado', sortable: false },
+        { key: 'tipo_emergencia', label: 'Tipo de Emergencia', sortable: false },
+        { key: 'end_date', label: 'Fecha Termino', sortable: false },
       ],
-      items: [
-        { id: 40, titulo: 'Dickerson', usuario:'name',estado: 'active', end_date: '2023-04-01' },
-        { id: 21, titulo: 'Larsen', usuario:'name',estado: 'active', end_date: '2023-04-01' },
-        { id: 89, titulo: 'Geneva', usuario:'name',estado: 'inactive', end_date: '2023-04-01'  },
-        { id: 38, titulo: 'Jami', usuario:'name',estado: 'active', end_date: '2023-04-01'  }
-      ]
     }
   },
+  async mounted() {
+    this.init()
+  },
+
+  methods:{
+    async init(){
+      await this.getNotes()
+    },
+
+    async getNotes(){
+      this.loading = true
+      await noteServices.notesList()
+        .then(({code , data, detail= null}) => {
+          if (code == 200){
+            // se agrega la propiedad index a cada objeto en notaList
+            this.notaList = data.notas.map((nota, index) => ({...nota, index: index + 1}))
+            this.$toasted.success('Notas cargadas correctamente')
+
+          }else{
+            this.$toasted.error('Error en el servidor del servidor')
+          }
+        })
+        .catch(() => {
+          this.$toasted.error('Error al obtener la informacion')
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+  }
 };
 </script>
 
@@ -77,6 +76,7 @@ export default {
   border-radius: 5px;
   padding: 0.5rem 1rem;
 }
+
 .table-responsive {
   overflow-x: auto;
 }

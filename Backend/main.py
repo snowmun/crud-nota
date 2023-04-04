27e7d2c01cc9fd5ce0d298a4a5b772c59c7e6f1c
snow <smashdojo3@gmail.com>
2023-04-04 03:38:16 -0400

    se crea ruta para comuna y region

--- Backend/main.py
+++ Backend/main.py
@@ -5,6 +5,8 @@ from routes.userService import router as users_router
 from routes.labelService import router as label_router
 from routes.labelNoteService import router as labelNoteService
 from routes.typeService import router as typeService
+from routes.communeService import router as communeService
+from routes.regionService import router as regionService
 
 app = FastAPI()
 
@@ -30,6 +32,8 @@ app.include_router(users_router)
 app.include_router(label_router)
 app.include_router(labelNoteService)
 app.include_router(typeService)
+app.include_router(communeService)
+app.include_router(regionService)
 
 
 
